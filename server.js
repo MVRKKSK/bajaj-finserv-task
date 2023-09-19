b7a50bb12497dfc18a1118e096cbbd690842f208
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2023-09-19 16:34:06 +0530

    added exception handling

--- server.js
+++ server.js
@@ -5,23 +5,38 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.post("/bfhl", (req, res) => {
-    const requestData = req.body.data;
+    try {
+        const requestData = req.body.data;
 
-    if (!requestData || !Array.isArray(requestData)) {
-        return res.status(400).json({ is_success: false, error: "Invalid data format" });
+        if (!requestData || !Array.isArray(requestData)) {
+            throw new Error("Invalid data format");
+        }
+
+        const numbers = requestData.filter(item => !isNaN(item));
+        const singleCharAlphabets = requestData.filter(item => typeof item === 'string' && item.length === 1 && isNaN(item));
+
+        if (requestData.some(item => typeof item === 'string' && item.length !== 1 && isNaN(item))) {
+            throw new Error("Invalid input format");
+        }
+
+        const alphabets = singleCharAlphabets;
+        const helper = [...alphabets];
+        const highest_alphabet = helper.length > 0 ? [helper.sort()[helper.length - 1]] : [];
+
+        const userResponse = {
+            is_success: true,
+            user_id: "miryala_kautilya_01032003",
+            email: "miryala.kautilya2020@vitbhopal.ac.in",
+            roll_number: "20BCE10503",
+            numbers,
+            alphabets: singleCharAlphabets,
+            highest_alphabet,
+        };
+
+        res.json(userResponse);
+    } catch (error) {
+        res.status(400).json({ is_success: false, error: error.message });
     }
-
-    const userResponse = {
-        is_success: true,
-        user_id: "miryala_kautilya_01032003",
-        email: "miryala.kautilya2020@vitbhopal.ac.in",
-        roll_number: "20BCE10503",
-        numbers: requestData.filter(item => !isNaN(item)),
-        alphabets: requestData.filter(item => isNaN(item)),
-        highest_alphabet: [requestData.filter(item => isNaN(item)).sort()[requestData.filter(item => isNaN(item)).length - 1]]
-    };
-
-    res.json(userResponse);
 });
 
 app.get("/bfhl", (req, res) => {
@@ -33,20 +48,10 @@ app.get("/bfhl", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("welcome to the bajaj task");
-})
-
-
-const port = 3000;
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is running on port ${port}`);
+    res.status(200).send("Hello, this is your root URL!");
 });
 
-
-// 1. Status
-// 2. User ID
-// 3. College Email ID
-// 4. College Roll Number
-// 5. Array for numbers
-// 6. Array for alphabets
-// 7. Highest Alphabet in the input array of alphabets [Refer to note in Annexure for more info]
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
